383ad84e64fe878a06d72cd5a6568c0b67e4b379
Anton <anton94xx@gmail.com>
2023-02-07 22:55:27 +0100

    ДЗ 19,21,23

---
+++ Homework23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
+void num (int n)
+{
+    int count = 1;
+    while (count <=n)
+    {
+        Console.WriteLine($"{count*count*count}");
+        count++;
+    }
+}
+Console.WriteLine("Введите положительное число: ");
+int number = int.Parse(Console.ReadLine());
+num(number);
