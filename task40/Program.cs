81b95f529b5819c85e8149bc3cab3b2929b30814
Anton <anton94xx@gmail.com>
2023-02-16 19:29:57 +0100

    семинар задача 40

---
+++ task40/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+
+double a = Input();
+double b = Input();
+double c = Input();
+
+if (CheckTriangle(a, b, c))
+{
+    Console.WriteLine("треугольник со сторонами такой длины может существовать");
+}
+
+else
+{
+    Console.WriteLine("треугольник со сторонами такой длины не может существовать");
+}
+
+bool CheckTriangle(double a, double b, double c)
+{
+    if ((a < (b + c)) && (b < (a + c)) && (c < (a + b))) 
+    return true;
+    else return false;
+}
+
+
+double Input()
+{
+    Console.WriteLine("Введите длину: ");
+    double res = double.Parse(Console.ReadLine()!);
+    return res;
+}
